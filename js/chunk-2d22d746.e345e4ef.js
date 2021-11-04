ec2b537d96e07e40c0e98226d66fd35f9a538d31
rafaeu749 <rafa749@gmail.com>
2021-11-04 01:43:29 -0300

    gh-pages

---
+++ js/chunk-2d22d746.e345e4ef.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,t,e){"use strict";e.r(t);var a=e("7a23"),c={class:"about"},s=Object(a["f"])("h1",null,"This is an about page",-1),u=[s];function o(n,t){return Object(a["t"])(),Object(a["e"])("div",c,u)}var b=e("6b0d"),d=e.n(b);const i={},r=d()(i,[["render",o]]);t["default"]=r}}]);
+//# sourceMappingURL=chunk-2d22d746.e345e4ef.js.map
